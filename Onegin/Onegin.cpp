037460aecd46adada2db7121ab50422675510659
vovina <vovalekhterev@gmail.com>
2020-09-02 23:01:54 +0300

    upd

--- Onegin/Onegin.cpp
+++ Onegin/Onegin.cpp
@@ -2,8 +2,12 @@
 #include <math.h>
 #include <stdio.h>
 #include <assert.h>
+#include <ctype.h>
 
 
+#define GREATER 1
+#define LESS -1
+#define EQUAL 0
 
 void add_line(/*char* line, */char*** lines_ptr, int counter){
     //int new_size;
@@ -25,8 +29,53 @@ void add_char(char toAdd, char** line_ptr,int counter){
 
 }
 
+char* next_letter(char* a){
 
+    if (*a == '\0'){
+        return nullptr;
+    }
 
+    else if (isalpha(*a)){
+        return a;
+    }
+    else{
+        return next_letter(a+1);
+    }
+}
+
+int lexicographicalCompare(char* a, char* b){
+    a = next_letter(a);
+    b = next_letter(b);
+    //printf("comparing %c and %c \n", *a, *b);
+    if (a == nullptr || b == nullptr){
+
+        if (a == nullptr && b == nullptr){
+            return EQUAL;
+        }
+
+        if (a == nullptr && b != nullptr){
+            return LESS;
+        }
+
+        if (a != nullptr && b == nullptr){
+            return GREATER;
+        }
+
+    }
+
+    if (*a > *b) {
+        //printf("%c > %c\n", *a, *b);
+        return GREATER;
+    }
+    else if (*a < *b) {
+        //printf("%c < %c\n", *a, *b);
+        return LESS;
+    }
+    else {
+        return lexicographicalCompare(a+1,b+1);
+    }
+
+}
 
 
 //int main(){
@@ -67,7 +116,6 @@ printf("\n%s\n", lines[2]);
 //printf("\n%c\n", lines[0][0]);
 */
 
-
 void readFile(char* name, char*** out_pointer){
 
     FILE* fp = fopen(name, "r");
@@ -98,7 +146,12 @@ void readFile(char* name, char*** out_pointer){
 
 int main(){
 
-    FILE* fp = fopen("test.txt", "r");
+    printf("----------\n");
+    int test = lexicographicalCompare("abc", "bk dsf glj");
+    printf("\ntest: %d\n", test);
+    printf("----------\n");
+
+    FILE* fp = fopen("test2.txt", "r");
     char current = getc(fp);
     char* line;
     char** lines;
@@ -107,29 +160,66 @@ int main(){
     int char_counter = 0;
     int line_counter = 0;
     while (current != EOF){
-
+        printf("%c",current);
         if (current == '\n'){
-            if (char_counter > 1){
-                lines = (char**)realloc(lines, (line_counter+1) * sizeof(char*));
-                lines[line_counter] = line;
-                printf(lines[line_counter]);
-                line_counter++;
-            }
+            lines = (char**)realloc(lines, (line_counter+1) * sizeof(char*));
+            //printf("AAA %s\n",line);
+            lines[line_counter] = line;
+            line_counter++;
+
 
             line = (char*)malloc(0);
             char_counter = 0;
-
         }
 
-        line = (char*)realloc(line, char_counter+1);
-        line[char_counter] = current;
-        char_counter++;
+        else {
+            line = (char*)realloc(line, char_counter+1);
+            line[char_counter] = current;
+            char_counter++;
+        }
         current = getc(fp);
+    }
+    //printf("\n%d", line_counter);
+    int indexes[line_counter+1];
+    for (int i = 0; i < line_counter; i++){
+            indexes[i] = i;
+    }
+
+    for (int i = 0; i < line_counter; i++){
+        for(int j = 0; j < line_counter-1; j++){
+            int comparison = lexicographicalCompare(lines[j], lines[j+1]);
+            //printf("%d\n", comparison);
+            if (comparison == GREATER){
+                printf("%s < %s\n", lines[j], lines[j+1]);
+
+
+                int t = indexes[j];
+                indexes[j] = indexes[j+1];
+                indexes[j+1] = t;
+
+            }
+            else if (comparison == GREATER){
+                printf("%s > %s\n", lines[j], lines[j+1]);
+            }
+        }
+        //printf("----------\n");
+        for (int i = 0; i < line_counter; i++){
+            //printf("%d\n",indexes[i]);
+            //printf("%s\n",lines[indexes[i]]);
+    }
+
+    }
+    printf("----------\n");
+    for (int i = 0; i < line_counter; i++){
+            //printf("%d\n",indexes[i]);
+            //printf("%s\n",lines[indexes[i]]);
+    }
+    for (int i = 0; i < line_counter; i++){
+            //printf("%d\n",indexes[i]);
+            printf("%s\n",lines[indexes[i]]);
     }
 
     return 0;
-
-
     //char **lines;
     //readFile("test.txt", &lines);
     //printf(lines[0]);
